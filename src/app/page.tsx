0815a511ba6cb99c8397f59cd78b596699f00e93
fxcx <facundo19burgos@gmail.com>
2024-06-22 22:14:38 -0300

    add component a page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button"
-// import Image from "next/image";
+import HomeSection from "@/sections/home/homeSection"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>page init</h1>
-      <div>
-        <Button>Click me</Button>
-      </div>
+    <main className="p-3">
+      <HomeSection/>
     </main>
   )
 }

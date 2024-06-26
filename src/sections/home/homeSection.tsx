4aa7958ef88c6c595a814ff6a3ac107c6fc2afcf
fxcx <facundo19burgos@gmail.com>
2024-06-26 16:39:49 -0300

    create homeSection para page/inicio

--- src/sections/home/homeSection.tsx
+++ src/sections/home/homeSection.tsx
@@ -1,11 +1,16 @@
-import { Button } from "@/components/ui/button"
+import Hero from "@/components/homeSection/hero"
+import SobreNosotros from "@/components/homeSection/sobreNosotros"
+
 
 export default function HomeSection() {
-    return(
-        <section className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-5xl font-bold">Service and shop <span className="text-blue-600">ARt</span></h1>
-            <p className="text-lg mt-4">somos profecionales en revestimiento colocasion</p>
-            <Button className="mt-5">Consulta nuestros Servicios</Button>
-        </section>
-    )
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <section>
+        <Hero />
+      </section>
+      <section>
+        <SobreNosotros />
+      </section>
+    </main>
+  )
 }
